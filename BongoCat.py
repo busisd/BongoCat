2b8250188c305a58177fe17b6368cd42ee2a29cc
Daniel B <busisd@carleton.edu>
2018-09-27 17:23:54 -0500

    Rainbow and lightswitch mode working, capped at 60fps, and icons showing up!

--- BongoCat.py
+++ BongoCat.py
@@ -1,15 +1,21 @@
 import pygame
+import random
 
 class BongoCatGameController():
 	def __init__(self):
-		self.CAT_NONE_DOWN_FILE = "images/BongoCatBothUp.png"
-		self.CAT_LEFT_DOWN_FILE = "images/BongoCatLeftDown.png"
-		self.CAT_RIGHT_DOWN_FILE = "images/BongoCatRightDown.png"
-		self.CAT_BOTH_DOWN_FILE = "images/BongoCatBothDown.png"
+		self.CAT_NONE_DOWN_FILE = "./images/BongoCatBothUp.png"
+		self.CAT_LEFT_DOWN_FILE = "./images/BongoCatLeftDown.png"
+		self.CAT_RIGHT_DOWN_FILE = "./images/BongoCatRightDown.png"
+		self.CAT_BOTH_DOWN_FILE = "./images/BongoCatBothDown.png"
 		self.CAT_NONE_DOWN_IMAGE = pygame.image.load(self.CAT_NONE_DOWN_FILE)
 		self.CAT_LEFT_DOWN_IMAGE = pygame.image.load(self.CAT_LEFT_DOWN_FILE)
 		self.CAT_RIGHT_DOWN_IMAGE = pygame.image.load(self.CAT_RIGHT_DOWN_FILE)
 		self.CAT_BOTH_DOWN_IMAGE = pygame.image.load(self.CAT_BOTH_DOWN_FILE)
+
+		self.BULB_FILE = "./images/bulb.png"
+		self.DISCO_FILE = "./images/disco.png"
+		self.BULB_IMAGE = pygame.image.load(self.BULB_FILE)
+		self.DISCO_IMAGE = pygame.image.load(self.DISCO_FILE)
 		
 		self.CAT_NONE_DOWN_IMAGE.set_colorkey((255,255,255))
 		self.CAT_LEFT_DOWN_IMAGE.set_colorkey((255,255,255))
@@ -26,8 +32,10 @@ class BongoCatGameController():
 		
 		self.bg_color = pygame.Color(255,255,255)
 		self.lightswitch_mode = False
+		self.rainbow_mode = False
 		
 		self.screen = None
+		self.game_clock = None
 		
 def main():
 	pygame.mixer.pre_init(44100, -16, 2, 2048)
@@ -37,6 +45,8 @@ def main():
 	game_control = BongoCatGameController()
 	pygame.display.set_caption("BONGO CAT")
 	game_control.screen = pygame.display.set_mode((800,450))
+	
+	game_control.game_clock = pygame.time.Clock()
 	
 	running = True
 	while(running):
@@ -57,6 +67,8 @@ def main():
 			if event.type == pygame.KEYDOWN:
 				if event.key==32:
 					game_control.lightswitch_mode = not game_control.lightswitch_mode
+				if event.key==114:
+					game_control.rainbow_mode = not game_control.rainbow_mode
 				if event.key==276:
 					_left_bongo_down(game_control)
 				if event.key==275:
@@ -66,8 +78,11 @@ def main():
 					_left_bongo_up(game_control)
 				if event.key==275:
 					_right_bongo_up(game_control)
+		if game_control.rainbow_mode:
+			_randomize_colors(game_control)
 
 		_update_screen(game_control)
+		game_control.game_clock.tick(60)
 	pygame.quit()
 
 def _left_bongo_down(game_control):
@@ -102,11 +117,32 @@ def _brighten_bg(game_control, brighten_amount):
 	game_control.bg_color.r = new_colors[0]
 	game_control.bg_color.g = new_colors[1]
 	game_control.bg_color.b = new_colors[2]
+
+def _randomize_colors(game_control):
+	new_colors = []
+	for color in [game_control.bg_color.r, game_control.bg_color.g, game_control.bg_color.b]:
+		change_amount = random.randrange(-4,5)
+		if color + change_amount <= 255 and color + change_amount >= 0:
+			color += change_amount
+		elif change_amount>0:
+			color = 255
+		else:
+			color = 0
+		new_colors.append(color)
+
+	game_control.bg_color.r = new_colors[0]
+	game_control.bg_color.g = new_colors[1]
+	game_control.bg_color.b = new_colors[2]
+
 	
 def _update_screen(game_control):
 	game_control.screen.fill(game_control.bg_color)
 	background_image = _choose_cat_image(game_control)
 	game_control.screen.blit(background_image, (0,0))
+	if game_control.lightswitch_mode:
+		game_control.screen.blit(game_control.BULB_IMAGE, (20,450-64-20))
+	if game_control.rainbow_mode:
+		game_control.screen.blit(game_control.DISCO_IMAGE, (20+64,450-64-20))
 	pygame.display.update()
 	
 def _choose_cat_image(game_control):
