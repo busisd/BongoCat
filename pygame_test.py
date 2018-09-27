d39b213281ad510ead577eb79559d3f98ce512b9
Daniel B <busisd@carleton.edu>
2018-09-27 15:08:55 -0500

    Made it so that it runs the main() method instead of just having the code unindented

--- pygame_test.py
+++ pygame_test.py
@@ -5,32 +5,32 @@
 		
 
 import pygame
-#def main():
-CAT_BOTH_UP_FILE = "BongoCatBothUp.png"
+def main():
+	CAT_BOTH_UP_FILE = "BongoCatBothUp.png"
 
-pygame.init()
-pygame.display.set_caption("BONGO CAT")
-screen = pygame.display.set_mode((800,450))
-screen.fill(pygame.Color(255,255,255))
-pygame.display.update()
+	pygame.init()
+	pygame.display.set_caption("BONGO CAT")
+	screen = pygame.display.set_mode((800,450))
+	screen.fill(pygame.Color(255,255,255))
+	pygame.display.update()
 
-running = True
+	running = True
 
-while(running):
-	for event in pygame.event.get():
-		print(event)
-		if event.type == pygame.QUIT:
-			running = False
-			
-		if event.type == pygame.MOUSEBUTTONDOWN:
-			if event.button==1:
-				screen.fill(pygame.Color(0,255,0))
+	while(running):
+		for event in pygame.event.get():
+			print(event)
+			if event.type == pygame.QUIT:
+				running = False
+				
+			if event.type == pygame.MOUSEBUTTONDOWN:
+				if event.button==1:
+					screen.fill(pygame.Color(0,255,0))
+					pygame.display.update()
+			if event.type == pygame.MOUSEBUTTONUP:
+				screen.fill(pygame.Color(255,255,255))
 				pygame.display.update()
-		if event.type == pygame.MOUSEBUTTONUP:
-			screen.fill(pygame.Color(255,255,255))
-			pygame.display.update()
 
-pygame.quit()
+	pygame.quit()
 
-
-#main()
+if __name__=="__main__":
+	main()
