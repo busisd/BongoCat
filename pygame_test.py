033cf1a347d6d84b16102e3085d39b628eb99519
Daniel B <busisd@carleton.edu>
2018-09-27 16:51:26 -0500

    Some cleanup, lightswitch mode works now. Also arrow key support

--- pygame_test.py
+++ pygame_test.py
@@ -11,6 +11,11 @@ class BongoCatGameController():
 		self.CAT_RIGHT_DOWN_IMAGE = pygame.image.load(self.CAT_RIGHT_DOWN_FILE)
 		self.CAT_BOTH_DOWN_IMAGE = pygame.image.load(self.CAT_BOTH_DOWN_FILE)
 		
+		self.CAT_NONE_DOWN_IMAGE.set_colorkey((255,255,255))
+		self.CAT_LEFT_DOWN_IMAGE.set_colorkey((255,255,255))
+		self.CAT_RIGHT_DOWN_IMAGE.set_colorkey((255,255,255))
+		self.CAT_BOTH_DOWN_IMAGE.set_colorkey((255,255,255))
+
 		self.BONGO_SOUND_LOW_FILE = "sounds/low_bongo.wav"
 		self.BONGO_SOUND_HIGH_FILE = "sounds/high_bongo.wav"
 		self.BONGO_SOUND_LOW = pygame.mixer.Sound(self.BONGO_SOUND_LOW_FILE)
@@ -18,6 +23,9 @@ class BongoCatGameController():
 		
 		self.left_down = False
 		self.right_down = False
+		
+		self.bg_color = pygame.Color(255,255,255)
+		self.lightswitch_mode = False
 		
 		self.screen = None
 		
@@ -33,28 +41,70 @@ def main():
 	running = True
 	while(running):
 		for event in pygame.event.get():
-			#print(event)
 			if event.type == pygame.QUIT:
 				running = False
 				
 			if event.type == pygame.MOUSEBUTTONDOWN:
 				if event.button==1:
-					game_control.left_down = True
-					game_control.BONGO_SOUND_LOW.play()
+					_left_bongo_down(game_control)
 				if event.button==3:
-					game_control.right_down = True
-					game_control.BONGO_SOUND_HIGH.play()
+					_right_bongo_down(game_control)
 			if event.type == pygame.MOUSEBUTTONUP:
 				if event.button==1:
-					game_control.left_down = False
+					_left_bongo_up(game_control)
 				if event.button==3:
-					game_control.right_down = False
+					_right_bongo_up(game_control)
+			if event.type == pygame.KEYDOWN:
+				if event.key==32:
+					game_control.lightswitch_mode = not game_control.lightswitch_mode
+				if event.key==276:
+					_left_bongo_down(game_control)
+				if event.key==275:
+					_right_bongo_down(game_control)
+			if event.type == pygame.KEYUP:
+				if event.key==276:
+					_left_bongo_up(game_control)
+				if event.key==275:
+					_right_bongo_up(game_control)
 
 		_update_screen(game_control)
 	pygame.quit()
 
+def _left_bongo_down(game_control):
+	game_control.left_down = True
+	game_control.BONGO_SOUND_LOW.play()
+	if game_control.lightswitch_mode:
+		_brighten_bg(game_control, -15)
+
+def _right_bongo_down(game_control):
+	game_control.right_down = True
+	game_control.BONGO_SOUND_HIGH.play()
+	if game_control.lightswitch_mode:
+		_brighten_bg(game_control, 15)
+
+def _left_bongo_up(game_control):
+	game_control.left_down = False
+
+def _right_bongo_up(game_control):
+	game_control.right_down = False
+		
+def _brighten_bg(game_control, brighten_amount):
+	new_colors = []
+	for color in [game_control.bg_color.r, game_control.bg_color.g, game_control.bg_color.b]:
+		if color + brighten_amount <= 255 and color + brighten_amount >= 0:
+			color += brighten_amount
+		elif brighten_amount>0:
+			color = 255
+		else:
+			color = 0
+		new_colors.append(color)
+
+	game_control.bg_color.r = new_colors[0]
+	game_control.bg_color.g = new_colors[1]
+	game_control.bg_color.b = new_colors[2]
+	
 def _update_screen(game_control):
-	game_control.screen.fill(pygame.Color(255,255,255))
+	game_control.screen.fill(game_control.bg_color)
 	background_image = _choose_cat_image(game_control)
 	game_control.screen.blit(background_image, (0,0))
 	pygame.display.update()
